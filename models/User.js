ec394d7fed7ce78468cd769c41ff5cbec94c42fb
princebansal42 <princebansal42@gmail.com>
2020-04-30 23:26:26 +0530

    User Model Added

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+            required: true,
+        },
+        user_type: {
+            type: String,
+            get: v => v.toLowerCase(),
+            set: v => v.toLowerCase(),
+            enum: ["admin", "customer"],
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+const 
